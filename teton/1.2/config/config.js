a6fbbd4ce2c89baa820eb411738e2ef011a7a93d
sylamarie <sylamariecumuyog@outlook.com>
2025-02-04 23:31:25 +0800

    api key

--- teton/1.2/config/config.js
+++ teton/1.2/config/config.js
@@ -1,6 +1,6 @@
 const LAT = "43.8866";
 const LON = "-111.6777";
-const APIKEY = "https://api.openweathermap.org/data/2.5/weather?lat=43.8866&lon=-111.6777&appid=3737816ef52c12e67b685e8f426fd701";
+const APIKEY = "3737816ef52c12e67b685e8f426fd701";
 
 const WEATHER_URL_STUB = "./data/weather.json";
 const WEATHER_URL_PROD = `https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&appid=${APIKEY}`;
